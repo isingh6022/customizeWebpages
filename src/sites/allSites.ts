fa626c528123169815759a4e0d2ba054c668d8f8
Inderpal Singh <inderpalsingh423@gmail.com>
2023-08-20 14:20:34 +0530

    deviantart - ctrl+space to open lists

--- src/sites/allSites.ts
+++ src/sites/allSites.ts
@@ -3,7 +3,8 @@ import { Personalization } from '@scripts/Personalization';
 import { alphacoders } from './wall.alphacoders.com';
 import { youtube } from './youtube.com';
 import { amazon } from './amazon';
+import { deviantArt } from './deviantart';
 
-const allSites: Personalization[] = [alphacoders, youtube, amazon];
+const allSites: Personalization[] = [alphacoders, youtube, amazon, deviantArt];
 
 export { allSites };
