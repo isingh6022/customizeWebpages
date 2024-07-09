0b89a7202d435a62af59a8b303c81a6defc0565d
ips21blank <inderpalsingh423@gmail.com>
2024-07-09 20:44:01 +0530

    Personalize local video file running in chrome

--- src/sites/allSites.ts
+++ src/sites/allSites.ts
@@ -2,9 +2,10 @@ import { Personalization } from '@scripts/Personalization';
 
 import { alphacoders } from './wall.alphacoders.com';
 import { youtube } from './youtube.com';
+import { videoFile } from './video-file';
 import { amazon } from './amazon';
 import { deviantArt } from './deviantart';
 
-const allSites: Personalization[] = [alphacoders, youtube, amazon, deviantArt];
+const allSites: Personalization[] = [alphacoders, youtube, videoFile, amazon, deviantArt];
 
 export { allSites };
